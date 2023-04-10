45f5549079b31c5bd7b790e2755c2f0de80bd877
Tate Berenbaum <tateflyer1@gmail.com>
2023-04-10 17:56:41 -0400

    chore: add deps & format

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import "../node_modules/bulma/css/bulma.css";
 
-createApp(App).mount('#app')
+createApp(App).mount("#app");
